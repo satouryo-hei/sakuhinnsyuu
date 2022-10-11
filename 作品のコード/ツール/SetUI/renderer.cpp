ad9b558e5603714cef1e698ccfeaf8865bf6dbf8
satouryo-hei <201011@st.yoshida-g.ac.jp>
2022-10-11 17:36:25 +0900

    作品です

--- 作品のコード/ツール/SetUI/renderer.cpp
+++ 作品のコード/ツール/SetUI/renderer.cpp
@@ -195,7 +195,7 @@ void CRenderer::Draw(void)
 	m_pD3DDevice->Clear(0,
 		NULL,
 		(D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER),
-		D3DCOLOR_RGBA(0, 0, 0, 0), 1.0f, 0);
+		D3DCOLOR_RGBA(125, 125, 125, 0), 1.0f, 0);
 
 	// Direct3D‚É‚æ‚é•`‰æ‚ÌŠJŽn
 	if (SUCCEEDED(m_pD3DDevice->BeginScene()))
